a4759561d32d0e02b36108a111ede7e2c1896abf
dvir-berko <berko.faibish.dvir@gmail.com>
2025-08-11 20:12:03 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,23 @@
-variable "aws_availability_zone" {
-  default = "us-east-1a"
+# -------- variables.tf --------
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1" # your requested default
 }
+
+variable "aws_profile" {
+  description = "AWS CLI/SSO profile name"
+  type        = string
+  default     = "my-aws"
+}
+
+variable "tags" {
+  description = "Default tags applied to all resources"
+  type        = map(string)
+  default = {
+    Project = "Terraform-Labs"
+    Owner   = "Dvir"
+    Managed = "terraform"
+  }
+}
+# -------- end --------
